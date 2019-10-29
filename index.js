14db3f269362cc0151d0f78d3c6c07a248370700
Aisha Rashed <aisha1rashed@gmail.com>
2019-10-29 19:04:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ let num1= 31;
 let num2 = 2;
 let multiply = num1 * num2; //multiply 2 numbers
 let random = Math.floor(Math.random() * 10) + 1; //generate a random //integer
-let mod = 10 % 6 ;
+let mod = 10 % 6 ; // the remainder
 let max =Math.max(1,5,10,20);
 // Write your code here
